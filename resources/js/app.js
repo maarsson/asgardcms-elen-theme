16d34d303c04a1ae4204d8e96babcc41f458b3fe
VMaarsson <viktor@maarsson.se>
2022-01-23 07:05:51 +0100

    Apply scripts from soruce

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,24 @@
+global.$ = require('jquery');
+global.jQuery = global.$;
+
+require('jquery-migrate');
+
+require('popper.js');
+require('bootstrap');
+require('jquery.animate-number');
+require('jquery.easing');
+require('magnific-popup');
+require('owl.carousel');
+require('scrollax');
+require('stellar.js/jquery.stellar.js');
+require('waypoints/lib/jquery.waypoints.js');
+require('waypoints/lib/shortcuts/sticky.js');
+
 require('./bootstrap');
+
+global.AOS = require('./theme/aos.js');
+require('./theme/bootstrap-datepicker.js');
+require('./theme/range.js');
+// require('./theme/map.js');
+
+require('./theme/main.js');
